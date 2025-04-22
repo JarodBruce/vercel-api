14b47154a4ffc360ddea97c3ca2c5ea728caea76
Noname <>
2025-04-23 05:53:38 +0900

    Change response message

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const name = req.query.name || 'world';
-  res.status(200).json({ message: `Hello, ${name}!` });
+  res.status(200).json({ TypeScript: `Hello, ${name}` });
 }
